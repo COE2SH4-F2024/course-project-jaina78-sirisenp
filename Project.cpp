5897171f90459c57b9ff7c29e3cfb280334769a0
jaina78 <jaina78@mcmaster.ca>
2024-11-26 12:29:08 -0500

    Second

--- Project.cpp
+++ Project.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 #define DELAY_CONST 100000
 
+#define HEIGHT 10
+#define LENGTH 20
+
 bool exitFlag;
 
 void Initialize(void);
@@ -15,7 +18,9 @@ void DrawScreen(void);
 void LoopDelay(void);
 void CleanUp(void);
 
-
+// Initialize the player object with objPos
+objPos player(5, 5, '@'); // Player starts at position (5, 5) with symbol '@'
+objPos otherObject(3, 3, '$'); // Another object at position (3, 3) with symbol '$'
 
 int main(void)
 {
@@ -56,6 +61,34 @@ void RunLogic(void)
 void DrawScreen(void)
 {
     MacUILib_clearScreen();    
+
+    // Iterate through the grid to draw the game board
+    for (int y = 0; y < HEIGHT; y++)  // Loop through rows
+    {
+        for (int x = 0; x < LENGTH; x++)  // Loop through columns
+        {
+            // Print border characters
+            if (y == 0 || y == HEIGHT - 1 || x == 0 || x == LENGTH - 1)
+            {
+                MacUILib_printf("#");  // Draw border
+            }
+            // Print player object at its position
+            else if (x == player.getObjPos().pos->x && y == player.getObjPos().pos->y)
+            {
+                MacUILib_printf("%c", player.getSymbol());
+            }
+            // Print another object at its position (e.g., $ object)
+            else if (x == otherObject.getObjPos().pos->x && y == otherObject.getObjPos().pos->y)
+            {
+                MacUILib_printf("%c", otherObject.getSymbol());
+            }
+            else
+            {
+                MacUILib_printf(" ");  // Empty space
+            }
+        }
+        MacUILib_printf("\n");
+    }
 }
 
 void LoopDelay(void)
